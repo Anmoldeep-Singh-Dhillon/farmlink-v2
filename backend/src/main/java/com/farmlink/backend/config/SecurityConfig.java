48e5c480db005a6cf1a7115b37b29b82db8bab22
apple <apple@Anmols-MacBook-Pro.local>
2026-05-02 11:03:43 +0530

    changes

--- backend/src/main/java/com/farmlink/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/farmlink/backend/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
             .sessionManagement(sm -> sm
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/api/auth/**","/actuator/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
